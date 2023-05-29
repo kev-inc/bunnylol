62c88912805c8b2bfdfb8c57f8139324c9f0062d
kev-inc <kevincxy96@gmail.com>
2023-05-29 18:09:22 +0000

    added more links

--- src/App.js
+++ src/App.js
@@ -12,6 +12,53 @@ const COMMAND = {
         url: "https://facebook.com/",
         searchurl: "https://www.facebook.com/search/top/?q="
     },
+    wa: {
+        name: "WhatsApp Web",
+        url: "https://web.whatsapp.com/"
+    },
+    gm: {
+        name: "Gmail",
+        url: "https://mail.google.com/mail/u/0",
+        searchurl: "https://mail.google.com/mail/u/"
+    },
+    gd: {
+        name: "Google Drive",
+        url: "https://drive.google.com/drive/u/0",
+        searchurl: "https://drive.google.com/drive/u/"
+    },
+    yt: {
+        name: "YouTube",
+        url: "https://youtube.com/",
+        searchurl: "https://www.youtube.com/results?search_query="
+    },
+    gh: {
+        name: "GitHub",
+        url: "https://github.com/",
+        searchurl: "https://www.github.com/search?q="
+    },
+    l: {
+        name: "Linkedin",
+        url: "https://linkedin.com/",
+    },
+    ig: {
+        name: "Instagram",
+        url: "https://instagram.com/",
+        searchurl: "https://instagram.com/"
+    },
+    n: {
+        name: "Netflix",
+        url: "https://netflix.com/",
+        searchurl: "https://www.netflix.com/search?q="
+    },
+    cal: {
+        name: "Google Calendar",
+        url: "https://calendar.google.com/calendar/r"
+    },
+    cineb: {
+        name: 'cineb.net',
+        url: 'https://cineb.net/',
+        searchurl: 'https://cineb.net/search/'
+    },
     DEFAULT: {
         name: "Default - Google Search",
         url: "https://google.com/",
