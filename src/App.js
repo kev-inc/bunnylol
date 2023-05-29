23e6d76d7f62aea7ae7cdcd82f16cf57c9b1c235
kev-inc <kevincxy96@gmail.com>
2023-05-29 17:51:51 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useSearchParams } from "react-router-dom";
 import './App.css';
 
+const COMMAND = {
+    g: {
+        name: "Google",
+        url: "https://google.com/",
+        searchurl: "https://www.google.com/search?q="
+    },
+    fb: {
+        name: "Facebook",
+        url: "https://facebook.com/",
+        searchurl: "https://www.facebook.com/search/top/?q="
+    },
+    DEFAULT: {
+        name: "Default - Google Search",
+        url: "https://google.com/",
+        searchurl: "https://www.google.com/search?q="
+    }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [searchParams] = useSearchParams();
+    const search = searchParams.get('search')
+    if (search !== null) {
+        const tokens = search.split(' ')
+        if (tokens.length > 0) {
+            const command = tokens[0]
+            if (command in COMMAND) {
+                let url = COMMAND[command]['url']
+                if (tokens.length > 1) {
+                    const query = tokens.slice(1)
+                    url = COMMAND[command]['searchurl'] + query.join(" ")
+                }
+                window.location.replace(url)
+            } else {
+                window.location.replace(COMMAND['DEFAULT']['searchurl'] + search)
+            }
+        }
+    }
+    return (
+        <div>
+        </div>
+    );
 }
 
 export default App;
